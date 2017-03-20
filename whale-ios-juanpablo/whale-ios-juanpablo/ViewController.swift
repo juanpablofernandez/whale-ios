1e723a3b1b5ef49687763c5feab0747271ff6a98
Jay <jaypee.1797@gmail.com>
2017-03-20 10:26:59 -0700

    Started Xcode project

---
+++ whale-ios-juanpablo/whale-ios-juanpablo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  whale-ios-juanpablo
+//
+//  Created by Jay on 3/20/17.
+//  Copyright © 2017 Juan Pablo Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
